0f68d1c3c091239b2f90b25aebcc0a610bae7280
dominik.zarfl <dominik.zarfl@avl.com>
2025-10-10 08:58:13 +0200

    Remove obsolete build artifacts and configuration files for ZERO_CHECK and ALL_BUILD in the Debug directory. This includes the deletion of various .tlog files and recipe files that are no longer needed, streamlining the build process and reducing clutter in the project structure.

--- 05-implementation/build_ci/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repos/dawproject_c/05-implementation
-# Build directory: D:/Repos/dawproject_c/05-implementation/build_ci
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Repos/dawproject_c/05-implementation/build_ci/test_data_red_include-b12d07c.cmake")
-subdirs("_deps/catch2-build")
-subdirs("_deps/pugixml-build")
--- 05-implementation/build_ci/_deps/pugixml-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repos/dawproject_c/05-implementation/build_ci/_deps/pugixml-src
-# Build directory: D:/Repos/dawproject_c/05-implementation/build_ci/_deps/pugixml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
